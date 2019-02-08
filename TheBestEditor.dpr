604bf04cba10918b392a8d2b9fee43af2a6dad53
IsmaelVaz <ismael.hvaz@gotmail.com>
2019-02-08 14:27:07 -0200

    First Commit

---
+++ TheBestEditor.dpr
@@ -0,0 +1,17 @@
+program TheBestEditor;
+
+uses
+  Forms,
+  Principal in 'Principal.pas' {frmEditorXSLT},
+  dlgConfirmReplace in 'dlgConfirmReplace.pas' {ConfirmReplaceDialog},
+  dlgSearchText in 'dlgSearchText.pas' {TextSearchDialog},
+  dlgReplaceText in 'dlgReplaceText.pas' {TextReplaceDialog};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmEditorXSLT, frmEditorXSLT);
+  Application.Run;
+end.
